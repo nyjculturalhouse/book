6e2575fd3f2813f4418ff6fdd674f0e25ef487fb
nyjculturalhouse <nyjculturalhouse@gmail.com>
2026-07-11 15:40:22 +0900

    Create util.js

---
+++ js/util.js
@@ -0,0 +1,19 @@
+const Utils = {
+  debounce(func, wait) {
+    let timeout;
+    return function executedFunction(...args) {
+      const later = () => {
+        clearTimeout(timeout);
+        func(...args);
+      };
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+    };
+  },
+  
+  formatDate(dateString) {
+    if (!dateString) return '-';
+    const date = new Date(dateString);
+    return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`;
+  }
+};
