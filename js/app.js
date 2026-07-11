7356296c3fbdab9d21c4fbdc0f5c2969544d242d
nyjculturalhouse <nyjculturalhouse@gmail.com>
2026-07-11 15:17:31 +0900

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// js/app.js
+import { callAPI } from './api.js';
+
+document.addEventListener('DOMContentLoaded', async () => {
+    console.log('소소책방 시스템 시작');
+    // 초기 데이터 로딩 테스트
+    // const data = await callAPI('getBooks', { page: 1 });
+});
